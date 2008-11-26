eacbd1493af86497ae7f12036463dcbf68c8e7dc
Corbin Simpson <MostAwesomeDude@gmail.com>
2008-11-26 04:06:33 +0000

    Reenable DarkNotify.

--- darklight/core.py
+++ darklight/core.py
@@ -308,43 +308,42 @@ class DarkDB:
 		else:
 			file.tth = pickle.loads(oldfile[4])
 			file.dirty = False
-#import os
 
-#import pyinotify
+try:
+	import pyinotify
 
-#import darkcache
+	class DarkEvent(pyinotify.ProcessEvent):
+	
+		def process_IN_CREATE(self, event):
+			path = os.path.join(event.path, event.name)
+			twisted.internet.reactor.callFromThread(
+				darkcache.DarkCache().folders.append, darkcache.DarkFile(path))
+	
+		def process_IN_DELETE(self, event):
+			pass
 
-#import twisted.internet.reactor
-
-#mask = (pyinotify.EventsCodes.IN_CREATE
-	#| pyinotify.EventsCodes.IN_DELETE)
-
-#class DarkEvent(pyinotify.ProcessEvent):
-
-	#def process_IN_CREATE(self, event):
-		#path = os.path.join(event.path, event.name)
-		#twisted.internet.reactor.callFromThread(
-			#darkcache.DarkCache().folders.append, darkcache.DarkFile(path))
-
-	#def process_IN_DELETE(self, event):
-		#pass
-
-#class DarkNotify:
-	#_state = {"n": None, "wm": pyinotify.WatchManager()}
-
-	#def __init__(self):
-		#self.__dict__ = self._state
-
-	#def start(self):
-		#self.n = pyinotify.ThreadedNotifier(self.wm, DarkEvent())
-		#self.n.setDaemon(True)
-		#self.n.start()
-
-	#def add(self, folders):
-		#print self.wm.add_watch(folders, mask, rec=True, auto_add=True)
-
-	#def stop(self):
-		#self.n.stop()
+	class DarkNotify:
+		_state = {"n": None, "wm": pyinotify.WatchManager()}
+	
+		mask = (pyinotify.EventsCodes.IN_CREATE |
+			pyinotify.EventsCodes.IN_DELETE)
+	
+		def __init__(self):
+			self.__dict__ = self._state
+	
+		def start(self):
+			self.n = pyinotify.ThreadedNotifier(self.wm, DarkEvent())
+			self.n.setDaemon(True)
+			self.n.start()
+	
+		def add(self, folders):
+			print self.wm.add_watch(folders, mask, rec=True,
+				auto_add=True)
+	
+		def stop(self):
+			self.n.stop()
+except ImportError:
+	pass
 
 class DarkTimer:
 	_state = {"clocks": dict(), "timeouts": dict()}
