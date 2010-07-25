29d16bbe63c67f27c03c8a5e2ab986d03cf3bdb5
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-07-24 23:21:29 -0700

    gui: Use status bar.

--- dl.py
+++ dl.py
@@ -26,8 +26,13 @@ class ClientLogic(object):
     def __init__(self):
         self.connections = set()
         self.factory = DarkClientFactory()
-
         self.factory.new_connection_handler = self.connected_callback
+
+        self.status_context = gui.get_widget("statusbar").get_context_id("")
+
+    def set_status(self, message):
+        gui.get_widget("statusbar").pop(self.status_context)
+        gui.get_widget("statusbar").push(self.status_context, message)
 
     def setup_signals(self, gui):
         gui.signal_connect("on_connect_clicked", self.connect)
@@ -49,12 +54,12 @@ class ClientLogic(object):
             error("Port must be a number!")
             return
 
-        print "Connecting to %s:%d" % (host, port)
+        self.set_status("Connecting to %s:%d" % (host, port))
 
         twisted.internet.reactor.connectTCP(host, port, self.factory, 5)
 
     def connected_callback(self, protocol):
-        print "Connected successfully!"
+        self.set_status("Connected successfully!")
 
         self.connections.add(protocol)
 
