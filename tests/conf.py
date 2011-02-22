85383bcb10d39047639c1720592f4dda3c92601a
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-02-21 19:58:04 -0800

    tests: Enable conf tests.
    Oh god my eyes.

--- tests/conf.py
+++ tests/conf.py
@@ -34,3 +34,6 @@ class TestDarkConf(unittest.TestCase):
         self.assertTrue(self.dc.getboolean("ssl", "enabled"))
         self.assertEqual(self.dc.get("ssl", "key"), "test.key")
         self.assertEqual(self.dc.get("ssl", "certificate"), "test.crt")
+
+if __name__ == "__main__":
+    unittest.main()
