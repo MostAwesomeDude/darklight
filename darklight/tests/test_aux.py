51e70024fcc55866556bacf77575f93e1686587a
Corbin Simpson <simpsoco@osuosl.org>
2011-04-26 15:22:56 -0700

    tests: Remove broken tests.

--- darklight/tests/test_aux.py
+++ darklight/tests/test_aux.py
@@ -1,33 +1,8 @@
 import base64
-import hashlib
 import random
 import unittest
 
-from darklight.aux.hash import DarkHMAC
 from darklight.aux.util import deserialize
-
-class WhirlpoolTest(unittest.TestCase):
-
-    def test_builtin(self):
-        strings = "", "The quick brown fox jumps over the lazy dog."
-        for string in strings:
-            self.assertEqual(hashlib.new("whirlpool", string).digest(),
-                darklight.aux.Whirlpool(string).digest())
-
-
-class HMACTest(unittest.TestCase):
-
-    def test_simple(self):
-        DarkHMAC("")
-
-    def test_builtin(self):
-        use_builtin_whirlpool = False
-        default = DarkHMAC("test")
-
-        use_builtin_whirlpool = True
-        other = DarkHMAC("test")
-
-        self.assertEqual(default, other)
 
 class UtilTest(unittest.TestCase):
 
