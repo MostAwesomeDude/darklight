e5e77810bebd6de0acc40131e4d916b2f3f1b41a
Corbin Simpson <simpsoco@osuosl.org>
2011-04-27 16:23:59 -0700

    factory: Make hashing properly lazy.

--- darklight/factory.py
+++ darklight/factory.py
@@ -3,9 +3,9 @@
 from base64 import b32encode
 import os
 
-import twisted.internet.protocol
+from twisted.internet.protocol import ServerFactory
 import twisted.internet.ssl
-import twisted.internet.task
+from twisted.internet.task import cooperate
 from twisted.internet import reactor
 from twisted.internet.endpoints import TCP4ClientEndpoint
 
@@ -21,7 +21,7 @@ try:
 except ImportError:
     inotify = False
 
-class DarkServerFactory(twisted.internet.protocol.ServerFactory):
+class DarkServerFactory(ServerFactory):
 
     protocol = DarkServerProtocol
 
@@ -30,28 +30,25 @@ class DarkServerFactory(twisted.internet.protocol.ServerFactory):
 
         self.db = DarkDB(self.dc.get("database", "url"))
 
-        timer = DarkTimer("hashing files offline")
-        self.update_library_now()
-        timer.stop()
-
         if inotify:
             self.prepare_notify()
-
-        else:
-            loop = twisted.internet.task.LoopingCall(self.cache.update_single)
-            loop.start(1.0)
 
         self.endpoint = TCP4ClientEndpoint(
             reactor, self.dc.get("passthrough", "host"),
             self.dc.getint("passthrough", "port"))
 
-    def update_library_now(self):
-        for folder, none in self.dc.items("folders"):
-            os.path.walk(folder, self.update_library_path, None)
+    def startFactory(self):
+        g = (self.db.update(DarkFile(f))
+            for f in self.library_update_iterator())
+        cooperate(g)
 
-    def update_library_path(self, chaff, directory, names):
-        for name in names:
-            self.db.update(DarkFile(os.path.join(directory, name)))
+    def library_update_iterator(self):
+        timer = DarkTimer("library update")
+        for folder, none in self.dc.items("folders"):
+            for directory, directories, files in os.walk(folder):
+                for name in files:
+                    yield os.path.join(directory, name)
+        timer.stop()
 
     def buildProtocol(self, addr):
         p = self.protocol(self.endpoint)
