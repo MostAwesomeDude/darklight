4bbb452dad22fd689cc87974892cd5bac9b63b7b
Corbin Simpson <simpsoco@osuosl.org>
2011-04-28 14:57:22 -0700

    tests: Add test vectors.

--- darklight/tests/test_tth.py
+++ darklight/tests/test_tth.py
@@ -2,6 +2,7 @@
 
 import os
 import unittest
+import tempfile
 
 from darklight.tth import Branch, Leaf, TTH
 
@@ -14,14 +15,53 @@ class TTHTestVectors(unittest.TestCase):
     """
 
     def test_devnull(self):
-        expected = ']\x9e\xd0\n\x03\x0ec\x8b\xdbu:j$\xfb\x90\x0eZc\xb8\xe7>l%\xb6'
+        expected = "]\x9e\xd0\n\x03\x0ec\x8b\xdbu:j$\xfb\x90\x0eZc\xb8\xe7>l%\xb6"
         t = TTH(thex=True)
         t.build_tree_from_path(os.devnull)
         self.assertEqual(t.top.hash, expected)
         self.assertEqual(t.top.size, 0)
 
+    def test_zero(self):
+        f = tempfile.NamedTemporaryFile()
+        f.write("\x00")
+        f.flush()
+        f.seek(0)
+
+        expected = ("\xaa\xbb\xcc\xa0\x84\xac\xec\xd0Q"
+             "\x1d\x1fb2\xa1{\xfa\xef\xa4A\xb2\x98.UH")
+        t = TTH(thex=True)
+        t.build_tree_from_path(f.name)
+        self.assertEqual(t.top.hash, expected)
+        self.assertEqual(t.top.size, 1)
+
+    def test_1024_a(self):
+        f = tempfile.NamedTemporaryFile()
+        f.write("A" * 1024)
+        f.flush()
+        f.seek(0)
+
+        expected = ("_\xbd\x0eb\xad\x01mYkw\xd1\xd2"
+            "\x88\x83\xb9O\xedx\xec\xba\xf4d\t\x14")
+        t = TTH(thex=True)
+        t.build_tree_from_path(f.name)
+        self.assertEqual(t.top.hash, expected)
+        self.assertEqual(t.top.size, 1024)
+
+    def test_1025_a(self):
+        f = tempfile.NamedTemporaryFile()
+        f.write("A" * 1025)
+        f.flush()
+        f.seek(0)
+
+        expected = ("~Y\x1c\x1c\xd8\xf2\xe6\x12\x1f\xdb"
+            "\xcd\x80q\xba'\x96&\xb7qd-\x10\xa3\xdb")
+        t = TTH(thex=True)
+        t.build_tree_from_path(f.name)
+        self.assertEqual(t.top.hash, expected)
+        self.assertEqual(t.top.size, 1025)
+
     def test_devnull_no_thex(self):
-        expected = '2\x93\xacc\x0c\x13\xf0$_\x92\xbb\xb1vn\x16\x16zNXI-\xdes\xf3'
+        expected = "2\x93\xacc\x0c\x13\xf0$_\x92\xbb\xb1vn\x16\x16zNXI-\xdes\xf3"
         t = TTH(thex=False)
         t.build_tree_from_path(os.devnull)
         self.assertEqual(t.top.hash, expected)
