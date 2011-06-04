d3c8887e39babf204218cdd0985c6720531b30d5
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-06-04 15:40:11 -0700

    curdl-new: Accept input.

--- curdl-new.py
+++ curdl-new.py
@@ -1,15 +1,40 @@
 #!/usr/bin/env python
 
 import curses
+from curses.textpad import Textbox
 
 from twisted.internet import reactor
+from twisted.internet.defer import Deferred
+from twisted.python import log
+log.startLogging(open("curdl.log", "w"))
 
 from darklight.client import DarkClient
+
+class AsyncTextbox(Textbox):
+    """
+    A Textbox which doesn't block the reactor.
+    """
+
+    def __init__(self, win, insert_mode=False):
+        Textbox.__init__(self, win, insert_mode)
+        self.completion_deferred = Deferred()
+
+    def increment(self, char, validate=None):
+        if validate:
+            char = validate(char)
+            if not char:
+                return
+        if not self.do_command(char):
+            reactor.callLater(0, self.completion_deferred.callback,
+                self.gather())
+        self.win.refresh()
 
 class Curdle(object):
     """
     Curses logic for a Darklight client.
     """
+
+    textbox = None
 
     def __init__(self):
         self.client = DarkClient()
@@ -21,15 +46,31 @@ class Curdle(object):
         self.status = self.screen.derwin(1, max_x, max_y - 1, 0)
         curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
-        self.servers = self.screen.derwin(max_y - 2, max_x, 0, 0)
+        self.servers = self.screen.derwin(max_y - 3, max_x, 0, 0)
+
+        self.entry = self.screen.derwin(1, max_x, max_y - 2, 0)
 
     def fileno(self):
         return 0
 
     def doRead(self):
         c = self.screen.getch()
-        if c == ord("q"):
+        if self.textbox:
+            self.textbox.increment(c)
+        elif c == ord("q"):
             reactor.stop()
+        elif c in (ord("\n"), curses.KEY_ENTER):
+            d = self.get_string()
+            @d.addCallback
+            def cb(s):
+                host, port = s.split(":")
+                d = self.client.add_server(host, int(port))
+                d.addCallback(lambda none: self.update_servers())
+            @d.addErrback
+            def eb(f):
+                self.set_status("Couldn't understand you!")
+        else:
+            log.msg("Unhandled character %d" % c)
 
     def logPrefix(self):
         return "Curdle"
@@ -67,9 +108,24 @@ class Curdle(object):
             self.servers.addstr(i, 0, str(server))
         self.servers.refresh()
 
+    def get_string(self):
+        """
+        Start a textbox.
+
+        Returns a Deferred which will fire with the result of the textbox.
+        """
+
+        self.entry.clear()
+        self.entry.refresh()
+        self.textbox = AsyncTextbox(self.entry)
+        d = self.textbox.completion_deferred
+        @d.addCallback
+        def cb(s):
+            self.textbox = None
+            return s
+        return d
+
 curdle = Curdle()
 curdle.start()
 reactor.addReader(curdle)
-d = curdle.client.add_server("localhost", 56789)
-d.addCallback(lambda chaff: curdle.update_servers())
 reactor.run()
