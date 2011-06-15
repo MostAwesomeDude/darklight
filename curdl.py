c56ede419c3b954b26fac2cd4aa1e79a235d9a9e
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-06-14 23:31:47 -0700

    Move curdl-new to curdl.
    Does everything the old one did.

--- curdl.py
+++ curdl.py
@@ -1,51 +1,131 @@
 #!/usr/bin/env python
 
-import urwid
+import curses
+from curses.textpad import Textbox
 
 from twisted.internet import reactor
+from twisted.internet.defer import Deferred
+from twisted.python import log
+log.startLogging(open("curdl.log", "w"))
 
 from darklight.client import DarkClient
 
-class ServerInfo(urwid.Text):
+class AsyncTextbox(Textbox):
+    """
+    A Textbox which doesn't block the reactor.
+    """
 
-    def __init__(self, server, **kwargs):
-        urwid.Text.__init__(self, "", **kwargs)
+    def __init__(self, win, insert_mode=False):
+        Textbox.__init__(self, win, insert_mode)
+        self.completion_deferred = Deferred()
 
-        self.server = server
-        d = server.get_remote_info()
+    def increment(self, char, validate=None):
+        if validate:
+            char = validate(char)
+            if not char:
+                return
+        if not self.do_command(char):
+            reactor.callLater(0, self.completion_deferred.callback,
+                self.gather())
+        self.win.refresh()
+
+class Curdle(object):
+    """
+    Curses logic for a Darklight client.
+    """
+
+    textbox = None
+
+    def __init__(self):
+        self.client = DarkClient()
+        self.screen = curses.initscr()
+        curses.start_color()
+
+        max_y, max_x = self.screen.getmaxyx()
+
+        self.status = self.screen.derwin(1, max_x, max_y - 1, 0)
+        curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+
+        self.servers = self.screen.derwin(max_y - 3, max_x, 0, 0)
+
+        self.entry = self.screen.derwin(1, max_x, max_y - 2, 0)
+
+    def fileno(self):
+        return 0
+
+    def doRead(self):
+        c = self.screen.getch()
+        if self.textbox:
+            self.textbox.increment(c)
+        elif c == ord("q"):
+            reactor.stop()
+        elif c in (ord("\n"), curses.KEY_ENTER):
+            d = self.get_string()
+            @d.addCallback
+            def cb(s):
+                host, port = s.split(":")
+                d = self.client.add_server(host, int(port))
+                d.addCallback(lambda none: self.update_servers())
+            @d.addErrback
+            def eb(f):
+                self.set_status("Couldn't understand you!")
+        else:
+            log.msg("Unhandled character %d" % c)
+
+    def logPrefix(self):
+        return "Curdle"
+
+    def connectionLost(self, reason):
+        self.stop()
+
+    def start(self):
+        self.screen.nodelay(True)
+        curses.noecho()
+        curses.cbreak()
+        self.screen.keypad(True)
+        self.screen.refresh()
+
+        self.set_status("Welcome to Darklight!")
+
+    def stop(self):
+        self.screen.keypad(False)
+        curses.nocbreak()
+        curses.echo()
+        curses.endwin()
+
+    def set_status(self, message):
+        y, x = self.status.getmaxyx()
+        if x > len(message):
+            message = message.center(x - 1, " ")
+        self.status.clear()
+        self.status.addnstr(0, 0, message, x,
+            curses.A_BOLD | curses.color_pair(1))
+        self.status.refresh()
+
+    def update_servers(self):
+        self.servers.clear()
+        for i, server in enumerate(self.client.connections):
+            self.servers.addstr(i, 0, str(server))
+        self.servers.refresh()
+
+    def get_string(self):
+        """
+        Start a textbox.
+
+        Returns a Deferred which will fire with the result of the textbox.
+        """
+
+        self.entry.clear()
+        self.entry.refresh()
+        self.textbox = AsyncTextbox(self.entry)
+        d = self.textbox.completion_deferred
         @d.addCallback
-        def cb(data):
-            self.api, self.version = data
-            self._update_server_info()
+        def cb(s):
+            self.textbox = None
+            return s
+        return d
 
-    def _update_server_info(self):
-        self.set_text("%s %s %s" %
-            (self.server, self.version, self.api))
-
-palette = [
-    ("header", "light green", "default"),
-]
-
-server_list = urwid.SimpleListWalker([])
-servers = urwid.ListBox(server_list)
-
-client = DarkClient()
-
-statusbar = urwid.Text("Welcome to DarkLight!", align="center")
-view = urwid.Frame(servers, footer=urwid.AttrMap(statusbar, "header"))
-
-def connect_to_server(host, port):
-    d = client.add_server(host, port)
-    d.addCallback(lambda p: server_list.append(ServerInfo(p)))
-
-reactor.callLater(5, connect_to_server, "localhost", 56789)
-
-def exit_on_q(i):
-    if i in ("q", "Q"):
-        for widget in server_list:
-            widget.server.transport.loseConnection()
-        raise urwid.ExitMainLoop()
-
-loop = urwid.MainLoop(view, palette, unhandled_input=exit_on_q,
-    event_loop=urwid.TwistedEventLoop())
-loop.run()
+curdle = Curdle()
+curdle.start()
+reactor.addReader(curdle)
+reactor.run()
